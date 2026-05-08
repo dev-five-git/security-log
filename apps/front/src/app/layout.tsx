925393a0177fe7eb28918d7a8e2ead791feb5359
indianapoly <hyeonlimgo5@gmail.com>
2026-05-08 17:45:31 +0900

    feature: 메인 페이지

--- apps/front/src/app/layout.tsx
+++ apps/front/src/app/layout.tsx
@@ -1,7 +1,27 @@
+import { globalCss } from '@devup-ui/react'
 import { resetCss } from '@devup-ui/reset-css'
 import type { Metadata } from 'next'
 
+import { Footer } from '@/components/layout/footer/Footer'
+import { Header } from '@/components/layout/header/Header'
+
 resetCss()
+
+globalCss({
+  a: {
+    textDecoration: 'none',
+    color: 'inherit',
+  },
+  fontFaces: [
+    {
+      fontFamily: 'Assacom FreeGothic',
+      src: `url('https://cdn.jsdelivr.net/gh/fonts-archive/AssacomFreeGothic/AssacomFreeGothic.woff2') format('woff2')`,
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+      fontDisplay: 'swap',
+    },
+  ],
+})
 
 export const metadata: Metadata = {
   title: 'Devfive',
@@ -15,7 +35,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
--- apps/front/src/app/page.tsx
+++ apps/front/src/app/page.tsx
@@ -1,9 +1,17 @@
-import { Box } from '@devup-ui/react'
+import { VStack } from '@devup-ui/react'
+
+import { AccidentCauseStatistics } from '@/components/pages/(home)/AccidentCauseStatistics/AccidentCauseStatistics'
+import { AccidentList } from '@/components/pages/(home)/AccidentList/AccidentList'
+import { MainBanner } from '@/components/pages/(home)/MainBanner'
+import { Service } from '@/components/pages/(home)/Service/Service'
 
 export default function HomePage() {
   return (
-    <Box bg="red" color="white">
-      HomePage
-    </Box>
+    <VStack>
+      <MainBanner />
+      <AccidentCauseStatistics />
+      <AccidentList />
+      <Service />
+    </VStack>
   )
 }
